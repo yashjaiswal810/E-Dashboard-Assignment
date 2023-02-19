47d18eee603b969dfb39e5ac98ff6fcc3cfd4777
Yash Jaiswal <yashjaiswal810@gmail.com>
2023-02-19 17:29:47 +0530

    first commit

---
+++ backend/db/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+});
+
+module.exports = mongoose.model("users",userSchema);
